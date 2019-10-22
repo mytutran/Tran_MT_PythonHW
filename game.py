d1d4ef74ee32e0085c48ff7e760ac2892666cfda
Tran <m_tran110573@fanshaweonline.ca>
2019-10-22 14:35:02 -0400

    all1

---
+++ game.py
@@ -0,0 +1,29 @@
+#import the random package so that we can generate a random choice
+from random import randint 
+
+#choices is an array => an array is a container that can hold multiple values.
+choices=["Rock","Paper","Scissors"]
+
+#set the computer variable to one of these choices
+computer= choices[randint(0,2)]
+
+# set up the game loop so that we don't have to restart all the time
+player = False
+while player is False:
+	#set player to True
+	player = input("Choose Rock, Paper or Scissors\n")
+
+	print("Computer chose", computer, "\n")
+	print("Player chose", player, "\n")
+
+	if player == "Quit":
+		exit()
+	elif computer == player:
+		print("Draw")
+
+
+
+
+	#need to check all of our conditions after checking for a tie.	
+	player = False
+	computer = choices[randint(0,2)]
