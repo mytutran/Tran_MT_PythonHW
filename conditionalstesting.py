e2e56cfde80bf7894cc2522427cddaf786e0c4e9
Tran <m_tran110573@ats-hr420-dm15.fc.ca>
2019-10-29 14:12:59 -0400

    final

--- conditionalstesting.py
+++ conditionalstesting.py
@@ -1,6 +1,8 @@
 #import the random package so that we can generate a random choice
 from random import randint 
 
+# set up some variables for player and AI lives
+
 #choices is an array => an array is a container that can hold multiple values.
 choices=["Rock","Paper","Scissors"]
 
@@ -12,36 +14,39 @@ player = False
 while player is False:
 	#set player to True
 	print("+++++++++++++++++++++++++++++++++++++++++")
+	print("Computer lives: ", computer_lives, "/5\n")
+	print("Player lives: ", player_lives, "/5\n")
 	print("Choose your weapon!\n")
 	print("+++++++++++++++++++++++++++++++++++++++++")
 
-	player = input("Choose Rock, Paper or Scissors\n")
+	if player = input("Choose Rock, Paper or Scissors\n")
 
-	print("Computer chose", computer, "\n")
-	print("Player chose", player, "\n")
+		print("Computer chose", computer, "\n")
+		print("Player chose", player, "\n")
+	else 	
 
-	if player == "Quit":
-		exit()
-	elif computer == player:
-		print("Draw!")
+			if player == "Quit":
+				exit()
+			elif computer == player:
+				print("Draw!")
 
-	elif (player == "Scissors" and computer == "Rock"):
-		print("You lose!")
-	
-	elif (player == "Scissors" and computer == "Paper"):
-		print("You win!")
+			elif (player == "Scissors" and computer == "Rock"):
+				print("You lose!")
+			
+			elif (player == "Scissors" and computer == "Paper"):
+				print("You win!")
 
-	elif (player == "Paper" and computer == "Rock"):
-		print("You win!")
+			elif (player == "Paper" and computer == "Rock"):
+				print("You win!")
 
-	elif (player == "Paper" and computer == "Scissors"):
-		print("You lose!")
+			elif (player == "Paper" and computer == "Scissors"):
+				print("You lose!")
 
-	elif (player == "Rock" and computer == "Scissors"):
-		print("You win!")
+			elif (player == "Rock" and computer == "Scissors"):
+				print("You win!")
 
-	elif (player == "Paper" and computer == "Scissors"):
-		print("You lose!")			
+			elif (player == "Paper" and computer == "Scissors"):
+				print("You lose!")			
 
 	else:
 		print("That's not a valid choice, try again.")			
